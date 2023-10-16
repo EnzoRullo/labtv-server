547c06e14f9c7e2b0617bd557cc1dbea4a74b828
EnzoRullo <lorenzo.rullo27@gmail.com>
2023-10-16 15:55:27 +0200

    labtv server for labtv project

---
+++ appunti.js
@@ -0,0 +1,10 @@
+/*
+{
+    "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBpcHBvQGdtYWlsLmNvbSIsImlhdCI6MTY4NDcwMzMyNywiZXhwIjoxNjg0NzA2OTI3LCJzdWIiOiIxIn0.zWBmq4G5LcFU09F1mfYd6SD2k0I8KCA5F0lsDpDLfQk",
+    "user": {
+        "email": "paperino@gmail.com",
+        "username": "paperino",
+        "id": 2
+    }
+}
+*/
